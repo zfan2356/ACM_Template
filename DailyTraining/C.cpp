f282de2a2441ac021eb27b4189e63a76f26e3a92
LAPTOP-BGI5ULO8\86150 <2356754953@qq.com>
2023-09-23 18:55:03 +0800

    模板修正

--- DailyTraining/C.cpp
+++ DailyTraining/C.cpp
@@ -1,46 +1,50 @@
 #include <bits/stdc++.h>
-
-using std::cin;
-using std::cout;
-using std::endl;
-using i64 = long long;
-
-template<typename A, typename B>
-inline std::ostream &operator<<(std::ostream &out, const std::pair <A, B> &p) {
-    return out << "(" << p.first << ", " << p.second << ")";
-}
-
-template<typename T>
-inline std::ostream &operator<<(std::ostream &out, const std::vector <T> &a) {
-    out << "[";
-    for (int i = 0; i < a.size(); i++) {
-        if (i) out << ',';
-        out << ' ' << a[i];
+using namespace std;
+const int N = 60;
+int g[N], c[N << 1], tt, a[N], b[N], inv[N], n, f[N][N << 1];
+const int P = 998244353;
+int power(int a, int b) {
+    int c = 1;
+    for (; b; b >>= 1) {
+        if (b & 1) c = 1ll * c * a % P;
+        a = 1ll * a * a % P;
     }
-    return out << " ]";
+    return c;
 }
-
-template<typename T>
-inline std::ostream &operator<<(std::ostream &out, const std::set <T> &a) {
-    return out << std::vector<T>(all(a));
-}
-
-constexpr int inf = 1e9;
-
-void solve() {
-
-
-}
-
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    int Case = 1;
-
-    while (Case--) {
-        solve();
+    scanf("%d", &n);
+    for (int i = 1; i <= n; ++i) {
+        scanf("%d%d", a + i, b + i);
+        c[++tt] = a[i];
+        c[++tt] = ++b[i];
     }
-
-    return 0;
+    sort(c + 1, c + tt + 1);
+    tt = unique(c + 1, c + tt + 1) - c - 1;
+    for (int i = 1; i <= n; ++i) {
+        a[i] = lower_bound(c + 1, c + tt + 1, a[i]) - c;
+        b[i] = lower_bound(c + 1, c + tt + 1, b[i]) - c;
+    }
+    for (int i = 1; i <= n; ++i) inv[i] = power(i, P - 2);
+    a[0] = 1, b[0] = tt + 1;
+    for (int i = 1; i <= tt; ++i) f[0][i] = 1;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = a[i]; j < b[i]; ++j) {
+            int len = c[j + 1] - c[j];
+            g[1] = len;
+            for (int k = 2; k <= i; ++k) g[k] = 1ll * g[k - 1] * (len + k - 1) % P * inv[k] % P;
+            for (int k = i - 1; k >= 0; --k) {
+                f[i][j] += 1ll * f[k][j + 1] * g[i - k] % P;//C(len + i - k - 1, i - k)
+                f[i][j] %= P;
+                if (j < a[k] || j >= b[k]) break;
+            }
+        }
+        for (int j = tt - 1; j; --j) {
+            f[i][j] += f[i][j + 1];
+            f[i][j] %= P;
+        }
+    }
+    int ans = f[n][1];
+    cout << ans << '\n';
+    for (int i = 1; i <= n; ++i) ans = 1ll * ans * power(c[b[i]] - c[a[i]], P - 2) % P;
+    printf("%d", ans);
 }
